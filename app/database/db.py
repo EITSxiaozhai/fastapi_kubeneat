cd9ee423f7735ffbca1c5ff94309785572f2ab49
_Exploit <eitsxiaozhai@gmail.com>
2026-05-14 14:29:02 +0800

    导包路径修改

--- app/database/db.py
+++ app/database/db.py
@@ -16,7 +16,7 @@ SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, expi
 
 
 def init_db() -> None:
-    from app.models import TaskRecord, User, UserSession  # noqa: F401
+    from app.models.models import TaskRecord, User, UserSession  # noqa: F401
 
     Base.metadata.create_all(bind=engine)
 
